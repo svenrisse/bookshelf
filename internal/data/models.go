e5754a0ec091f38cdd260d379384fa926180aa07
Sven <svenrisse0@gmail.com>
2024-04-06 12:00:21 +0200

    feat: add models file

---
+++ internal/data/models.go
@@ -0,0 +1,17 @@
+package data
+
+import (
+	"database/sql"
+	"errors"
+)
+
+var (
+	ErrRecordNotFound = errors.New("record not found")
+	ErrEditConflict   = errors.New("edit conflict")
+)
+
+type Models struct{}
+
+func NewModels(db *sql.DB) Models {
+	return Models{}
+}
