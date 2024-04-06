ea0bb8c3335797aed4525a9507deb8bc507a1303
Sven <svenrisse0@gmail.com>
2024-04-06 11:59:43 +0200

    feat: add vcs

---
+++ internal/vcs/vcs.go
@@ -0,0 +1,36 @@
+package vcs
+
+import (
+	"fmt"
+	"runtime/debug"
+)
+
+func Version() string {
+	var (
+		time     string
+		revision string
+		modified bool
+	)
+
+	bi, ok := debug.ReadBuildInfo()
+	if ok {
+		for _, s := range bi.Settings {
+			switch s.Key {
+			case "vcs.time":
+				time = s.Value
+			case "vcs.revision":
+				revision = s.Value
+			case "vcs.modified":
+				if s.Value == "true" {
+					modified = true
+				}
+			}
+		}
+	}
+
+	if modified {
+		return fmt.Sprintf("%s-%s-dirty", time, revision)
+	}
+
+	return fmt.Sprintf("%s-%s", time, revision)
+}
