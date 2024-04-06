c70c574672f2c2bebca7779e8af0777f750e7a0f
Sven <svenrisse0@gmail.com>
2024-04-06 15:06:48 +0200

    fix: wrong table name

--- migrations/000001_create_books_table.up.sql
+++ migrations/000001_create_books_table.up.sql
@@ -1,4 +1,4 @@
-CREATE TABLE IF NOT EXISTS movies (
+CREATE TABLE IF NOT EXISTS books (
     id bigserial PRIMARY KEY,  
     created_at timestamp(0) with time zone NOT NULL DEFAULT NOW(),
     title text NOT NULL,
