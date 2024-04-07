fde035dcad48ec54d211dc73b6ab0ab3eb103909
Sven <svenrisse0@gmail.com>
2024-04-07 17:39:42 +0200

    fix: comment out not ready routes for now

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -18,7 +18,11 @@ func (app *application) routes() http.Handler {
 
 	router.HandlerFunc(http.MethodGet, "/v1/healthcheck", app.healthcheckHandler)
 
+	// router.HandlerFunc(http.MethodGet, "/v1/books", app.requirePermission("books:read", app.listBookHandler))
 	router.HandlerFunc(http.MethodPost, "/v1/books", app.requirePermission("books:write", app.createBookHandler))
+	// router.HandlerFunc(http.MethodGet, "/v1/books/:id", app.requirePermission("books:read", app.showBookHandler))
+	// router.HandlerFunc(http.MethodPatch, "/v1/books/:id", app.requirePermission("books:write", app.updateBookHandler))
+	// router.HandlerFunc(http.MethodDelete, "/v1/books/:id", app.requirePermission("books:write", app.deleteBookHandler))
 
 	router.HandlerFunc(http.MethodPost, "/v1/users", app.registerUserHandler)
 	router.HandlerFunc(http.MethodPut, "/v1/users/activated", app.activateUserHandler)
