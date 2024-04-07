5d9caaa9cf5f86ddce5bb7938d0b6a45255330b9
Sven <svenrisse0@gmail.com>
2024-04-07 12:43:42 +0200

    feat: add background helper

--- cmd/api/helpers.go
+++ cmd/api/helpers.go
@@ -96,3 +96,18 @@ func (app *application) readJSON(w http.ResponseWriter, r *http.Request, dst any
 
 	return nil
 }
+
+func (app *application) background(fn func()) {
+	app.wg.Add(1)
+	go func() {
+		defer app.wg.Done()
+
+		defer func() {
+			if err := recover(); err != nil {
+				app.logger.Error(fmt.Sprintf("%v", err))
+			}
+		}()
+
+		fn()
+	}()
+}
