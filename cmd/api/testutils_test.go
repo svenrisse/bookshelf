79949238824c2db5571e4af7837b817405ee6be1
Sven <svenrisse0@gmail.com>
2024-04-08 21:23:13 +0200

    refactor(newTestServer): remove unused vars for now

--- cmd/api/testutils_test.go
+++ cmd/api/testutils_test.go
@@ -5,7 +5,6 @@ import (
 	"io"
 	"log/slog"
 	"net/http"
-	"net/http/cookiejar"
 	"net/http/httptest"
 	"testing"
 
@@ -26,18 +25,7 @@ type testServer struct {
 }
 
 func newTestServer(t *testing.T, h http.Handler) *testServer {
-	ts := httptest.NewServer(h)
-
-	jar, err := cookiejar.New(nil)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	ts.Client().Jar = jar
-
-	ts.Client().CheckRedirect = func(req *http.Request, via []*http.Request) error {
-		return http.ErrUseLastResponse
-	}
+	ts := httptest.NewTLSServer(h)
 
 	return &testServer{ts}
 }
@@ -54,7 +42,14 @@ func (ts *testServer) get(t *testing.T, urlPath string) (int, http.Header, strin
 		t.Fatal(err)
 	}
 
-	return rs.StatusCode, rs.Header, string("")
+	defer rs.Body.Close()
+	body, err := io.ReadAll(rs.Body)
+	if err != nil {
+		t.Fatal(err)
+	}
+	body = bytes.TrimSpace(body)
+
+	return rs.StatusCode, rs.Header, string(body)
 }
 
 func (ts *testServer) post(t *testing.T, urlPath string, body io.Reader) (int, http.Header, string) {
@@ -64,6 +59,7 @@ func (ts *testServer) post(t *testing.T, urlPath string, body io.Reader) (int, h
 	}
 
 	rq.Header.Set("Authorization", "Bearer Q5KJHXE3TJ3BUQRFWYYCAFSJDQ")
+
 	rs, err := ts.Client().Do(rq)
 	if err != nil {
 		t.Fatal(err)
