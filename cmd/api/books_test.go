e602a3bca618772a829e1509824d0ef0e4df923b
Sven <svenrisse0@gmail.com>
2024-04-08 21:21:52 +0200

    test(books): createBookHandler test in working state

--- cmd/api/books_test.go
+++ cmd/api/books_test.go
@@ -4,56 +4,55 @@ import (
 	"bytes"
 	"encoding/json"
 	"net/http"
+	"net/http/httptest"
 	"testing"
 
 	"github.com/svenrisse/bookshelf/internal/assert"
+	"github.com/svenrisse/bookshelf/internal/models"
 )
 
 func TestCreateBookHandler(t *testing.T) {
 	app := newTestApplication(t)
 
-	ts := newTestServer(t, app.routes())
+	w := httptest.NewRecorder()
 
-	const (
-		validTitle  = "The Hobbit"
-		validAuthor = "J.R.R Tolkien"
-		validYear   = 1932
-		validPages  = 300
-	)
+	validBook := models.Book{
+		Title:  "The Hobbit",
+		Author: "J.R.R. Tolkien",
+		Year:   1932,
+		Pages:  300,
+		Genres: []string{"Fantasy", "Children's Literature"},
+	}
 
 	tests := []struct {
-		title    string
-		author   string
-		year     int
-		pages    int
-		genres   []string
+		book     models.Book
 		name     string
 		wantCode int
 		wantBody string
 	}{
 		{
-			name:     "Valid Request",
+			name: "Valid Request",
+			book: models.Book{
+				Title:  validBook.Title,
+				Author: validBook.Author,
+				Year:   validBook.Year,
+				Pages:  validBook.Pages,
+				Genres: validBook.Genres,
+			},
 			wantCode: http.StatusCreated,
-			title:    validTitle,
-			author:   validAuthor,
-			year:     validYear,
-			pages:    validPages,
-			genres:   []string{"Fantasy", "Children's Literature"},
 		},
 	}
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			b, err := json.Marshal(map[string]any{"title": tt.title, "author": tt.author, "year": tt.year, "pages": tt.pages, "genres": tt.genres})
+			b, err := json.Marshal(map[string]any{"title": tt.book.Title, "author": tt.book.Author, "year": tt.book.Year, "pages": tt.book.Pages, "genres": tt.book.Genres})
 			if err != nil {
 				t.Fatal(err)
 			}
-			code, header, body := ts.post(t, "/v1/books", bytes.NewBuffer(b))
+			req := httptest.NewRequest(http.MethodPost, "/v1/books", bytes.NewBuffer(b))
+			app.createBookHandler(w, req)
 
-			t.Log(code)
-			t.Log(header)
-			t.Log(body)
-			assert.Equal(t, code, tt.wantCode)
+			assert.Equal(t, w.Result().StatusCode, tt.wantCode)
 		})
 	}
 }
