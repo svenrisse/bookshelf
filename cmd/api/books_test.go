4343327abd4153d7426372088b1bd3b06ef8a075
Sven <svenrisse0@gmail.com>
2024-04-09 10:37:04 +0200

    test(books): add more tests for validateBook

--- cmd/api/books_test.go
+++ cmd/api/books_test.go
@@ -6,6 +6,7 @@ import (
 	"net/http"
 	"net/http/httptest"
 	"testing"
+	"time"
 
 	"github.com/svenrisse/bookshelf/internal/assert"
 	"github.com/svenrisse/bookshelf/internal/models"
@@ -27,18 +28,93 @@ func TestValidateBookHandler(t *testing.T) {
 		wantError map[string]string
 	}{
 		{
-			name:      "valid Book",
+			name:      "valid book",
 			book:      validBook,
 			wantError: nil,
 		},
 		{
-			name: "invalid Title", book: models.Book{
+			name: "missing title", book: models.Book{
 				Title:  "",
 				Author: validBook.Author,
 				Year:   validBook.Year,
 				Pages:  validBook.Pages,
 				Genres: validBook.Genres,
 			}, wantError: map[string]string{"title": "must be provided"},
+		},
+		{
+			name: "missing author", book: models.Book{
+				Title:  validBook.Title,
+				Author: "",
+				Year:   validBook.Year,
+				Pages:  validBook.Pages,
+				Genres: validBook.Genres,
+			}, wantError: map[string]string{"author": "must be provided"},
+		},
+		{
+			name: "missing pages", book: models.Book{
+				Title:  validBook.Title,
+				Author: validBook.Author,
+				Year:   validBook.Year,
+				Pages:  0,
+				Genres: validBook.Genres,
+			}, wantError: map[string]string{"pages": "must be provided"},
+		},
+		{
+			name: "negative pages", book: models.Book{
+				Title:  validBook.Title,
+				Author: validBook.Author,
+				Year:   validBook.Year,
+				Pages:  -120,
+				Genres: validBook.Genres,
+			}, wantError: map[string]string{"pages": "must be a positive integer"},
+		},
+		{
+			name: "missing year", book: models.Book{
+				Title:  validBook.Title,
+				Author: validBook.Author,
+				Year:   0,
+				Pages:  validBook.Pages,
+				Genres: validBook.Genres,
+			}, wantError: map[string]string{"year": "must be provided"},
+		},
+		{
+			name: "year in future", book: models.Book{
+				Title:  validBook.Title,
+				Author: validBook.Author,
+				Year:   int32(time.Now().Year() + 1),
+				Pages:  validBook.Pages,
+				Genres: validBook.Genres,
+			}, wantError: map[string]string{"year": "must not be in the future"},
+		},
+		{
+			name: "missing genres", book: models.Book{
+				Title:  validBook.Title,
+				Author: validBook.Author,
+				Year:   validBook.Year,
+				Pages:  validBook.Pages,
+				Genres: nil,
+			}, wantError: map[string]string{"genres": "must be provided"},
+		},
+		{
+			name: "more than 10 genres", book: models.Book{
+				Title:  validBook.Title,
+				Author: validBook.Author,
+				Year:   validBook.Year,
+				Pages:  validBook.Pages,
+				Genres: []string{
+					"Genre",
+					"Genre",
+					"Genre",
+					"Genre",
+					"Genre",
+					"Genre",
+					"Genre",
+					"Genre",
+					"Genre",
+					"Genre",
+					"Genre",
+				},
+			}, wantError: map[string]string{"genres": "must not contain more than 10 genres"},
 		},
 	}
 
@@ -70,7 +146,15 @@ func TestCreateBookHandler(t *testing.T) {
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			b, err := json.Marshal(map[string]any{"title": tt.book.Title, "author": tt.book.Author, "year": tt.book.Year, "pages": tt.book.Pages, "genres": tt.book.Genres})
+			b, err := json.Marshal(
+				map[string]any{
+					"title":  tt.book.Title,
+					"author": tt.book.Author,
+					"year":   tt.book.Year,
+					"pages":  tt.book.Pages,
+					"genres": tt.book.Genres,
+				},
+			)
 			if err != nil {
 				t.Fatal(err)
 			}
