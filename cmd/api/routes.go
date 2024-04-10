d0167d67acbe11905144a93fe69c80ff11939450
Sven <svenrisse0@gmail.com>
2024-04-10 19:48:27 +0200

    fix(routes): POST /v1/books needs :write perm

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -19,7 +19,11 @@ func (app *application) routes() http.Handler {
 	router.HandlerFunc(http.MethodGet, "/v1/healthcheck", app.healthcheckHandler)
 
 	// router.HandlerFunc(http.MethodGet, "/v1/books", app.requirePermission("books:read", app.listBookHandler))
-	router.HandlerFunc(http.MethodPost, "/v1/books", app.requirePermission("books:read", app.createBookHandler))
+	router.HandlerFunc(
+		http.MethodPost,
+		"/v1/books",
+		app.requirePermission("books:write", app.createBookHandler),
+	)
 	// router.HandlerFunc(http.MethodGet, "/v1/books/:id", app.requirePermission("books:read", app.showBookHandler))
 	// router.HandlerFunc(http.MethodPatch, "/v1/books/:id", app.requirePermission("books:write", app.updateBookHandler))
 	// router.HandlerFunc(http.MethodDelete, "/v1/books/:id", app.requirePermission("books:write", app.deleteBookHandler))
@@ -27,7 +31,11 @@ func (app *application) routes() http.Handler {
 	router.HandlerFunc(http.MethodPost, "/v1/users", app.registerUserHandler)
 	router.HandlerFunc(http.MethodPut, "/v1/users/activated", app.activateUserHandler)
 
-	router.HandlerFunc(http.MethodPost, "/v1/tokens/authentication", app.createAuthenticationTokenHandler)
+	router.HandlerFunc(
+		http.MethodPost,
+		"/v1/tokens/authentication",
+		app.createAuthenticationTokenHandler,
+	)
 
 	router.Handler(http.MethodGet, "/v1/debug/vars", expvar.Handler())
 
