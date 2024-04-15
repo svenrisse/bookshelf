b2d2e24b26b3a91369394568a0a3f0acf5f5c8a9
Sven <svenrisse0@gmail.com>
2024-04-15 16:51:22 +0200

    migrations(5): move reviews into relation table

--- migrations/000005_create_users_books_relation.down.sql
+++ migrations/000005_create_users_books_relation.down.sql
@@ -1,2 +1,1 @@
 DROP TABLE IF EXISTS usersBooksRelation;
-DROP TABLE IF EXISTS reviews;
--- migrations/000005_create_users_books_relation.up.sql
+++ migrations/000005_create_users_books_relation.up.sql
@@ -1,26 +1,16 @@
-CREATE TABLE IF NOT EXISTS reviews (
-  id bigserial PRIMARY KEY,
-  bookId bigserial,
-  userId bigserial,
-  rating FLOAT(2),
-  body text,
-  created_at timestamp(0) with time zone NOT NULL DEFAULT NOW(),
-  version integer NOT NULL DEFAULT 1,
-
-  FOREIGN KEY (bookId) REFERENCES books(id),
-  FOREIGN KEY (userId) REFERENCES users(id)
-);
-
 CREATE TABLE IF NOT EXISTS usersBooksRelation (
+  id bigserial PRIMARY KEY,
   bookId bigserial,
   usersId bigserial, 
   reviewId bigserial,
   read BOOLEAN NOT NULL,
+  rating FLOAT(2),
+  reviewBody text,
   added_at timestamp(0) with time zone NOT NULL DEFAULT NOW(),
   date_read timestamp(0) with time zone,
+  reviewed_at timestamp(0) with time zone,
 
   FOREIGN KEY (bookId) REFERENCES books(id),
   FOREIGN KEY (usersId) REFERENCES users(id),
-  FOREIGN KEY (reviewId) REFERENCES reviews(id),
   UNIQUE (bookId, usersId)
 );
