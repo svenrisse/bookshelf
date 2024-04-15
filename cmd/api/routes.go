a704e755bc64e5c3144bb7e91ba6031f7295671f
Sven <svenrisse0@gmail.com>
2024-04-15 21:18:56 +0200

    feat(routes): reorder and add usersBooks routes

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -17,6 +17,11 @@ func (app *application) routes() http.Handler {
 	router.ServeFiles("/docs/*filepath", http.Dir("docs"))
 
 	router.HandlerFunc(http.MethodGet, "/v1/healthcheck", app.healthcheckHandler)
+	router.Handler(http.MethodGet, "/v1/debug/vars", expvar.Handler())
+
+	router.HandlerFunc(http.MethodGet, "/v1/auth/:provider/callback", app.AuthCallbackFunction)
+	router.HandlerFunc(http.MethodGet, "/v1/auth/:provider/logout", app.AuthLogout)
+	router.HandlerFunc(http.MethodGet, "/v1/auth/:provider", app.Auth)
 
 	router.HandlerFunc(http.MethodGet, "/v1/books", app.listBooksHandler)
 	router.HandlerFunc(http.MethodPost, "/v1/books", app.createBookHandler)
@@ -24,11 +29,10 @@ func (app *application) routes() http.Handler {
 	router.HandlerFunc(http.MethodPatch, "/v1/books/:id", app.updateBookHandler)
 	router.HandlerFunc(http.MethodDelete, "/v1/books/:id", app.deleteBookHandler)
 
-	router.HandlerFunc(http.MethodGet, "/v1/auth/:provider/callback", app.AuthCallbackFunction)
-	router.HandlerFunc(http.MethodGet, "/v1/auth/:provider/logout", app.AuthLogout)
-	router.HandlerFunc(http.MethodGet, "/v1/auth/:provider", app.Auth)
-
-	router.Handler(http.MethodGet, "/v1/debug/vars", expvar.Handler())
+	router.HandlerFunc(http.MethodGet, "/v1/:userid/books")
+	router.HandlerFunc(http.MethodPost, "/v1/:userid/books")
+	router.HandlerFunc(http.MethodPatch, "/v1/:userid/books/:bookid")
+	router.HandlerFunc(http.MethodDelete, "/v1/:userid/books/:bookid")
 
 	return app.metrics(app.recoverPanic(app.enableCORS(app.rateLimit((router)))))
 }
