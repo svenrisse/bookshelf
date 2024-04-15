834c6c473668fe0a86b15aed14dadf34ca875316
Sven <svenrisse0@gmail.com>
2024-04-15 19:39:25 +0200

    feat(UserBookModel): add Update and Delete Methods

--- internal/models/usersBooks.go
+++ internal/models/usersBooks.go
@@ -3,6 +3,7 @@ package models
 import (
 	"context"
 	"database/sql"
+	"errors"
 	"time"
 )
 
@@ -16,6 +17,7 @@ type UserBook struct {
 	Read       bool      `json:"read"`
 	Rating     float32   `json:"rating"`
 	ReviewBody string    `json:"review_body"`
+	Version    int32     `json:"-"`
 }
 
 type UserBookModel struct {
@@ -34,4 +36,57 @@ func (ub UserBookModel) Insert(userBook *UserBook) error {
 	defer cancel()
 
 	return ub.DB.QueryRowContext(ctx, query, args...).Scan(&userBook.ID, &userBook.CreatedAt)
+}
+
+func (ub UserBookModel) Update(userBook *UserBook) error {
+	query := `
+    UPDATE usersBooksRelation 
+    SET read = $1, rating = $2, reviewBody = $2, date_read = $3, date_reviewed_at = $4, version = version + 1
+    WHERE id = $5 AND version = $6
+    RETURNING version`
+
+	args := []any{
+		userBook.Read, userBook.Rating, userBook.ReviewBody, userBook.ReadAt, userBook.ReviewedAt, userBook.Version, userBook.ID, userBook.Version,
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	err := ub.DB.QueryRowContext(ctx, query, args...).Scan(&userBook.Version)
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			return ErrEditConflict
+		}
+		return err
+	}
+
+	return nil
+}
+
+func (ub UserBookModel) Delete(id int64) error {
+	if id < 1 {
+		return ErrRecordNotFound
+	}
+
+	query := `DELETE FROM usersBooksRelation
+    WHERE id = $1`
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	result, err := ub.DB.ExecContext(ctx, query, id)
+	if err != nil {
+		return err
+	}
+
+	rowsAffected, err := result.RowsAffected()
+	if err != nil {
+		return err
+	}
+
+	if rowsAffected == 0 {
+		return ErrRecordNotFound
+	}
+
+	return nil
 }
