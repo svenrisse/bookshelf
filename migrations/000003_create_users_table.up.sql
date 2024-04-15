c08c4a3cf47d4ffc372b76a6a27481c6638fbdad
Sven <svenrisse0@gmail.com>
2024-04-15 11:42:36 +0200

    migrations: refactor to use oauth

--- migrations/000003_create_users_table.up.sql
+++ migrations/000003_create_users_table.up.sql
@@ -2,8 +2,6 @@ CREATE TABLE IF NOT EXISTS users (
     id bigserial PRIMARY KEY,
     created_at timestamp(0) with time zone NOT NULL DEFAULT NOW(),
     name text NOT NULL,
-    email citext UNIQUE NOT NULL,
-    password_hash bytea NOT NULL,
-    activated bool NOT NULL,
-    version integer NOT NULL DEFAULT 1
+    avatar text, 
+    provider text NOT NULL
 );
