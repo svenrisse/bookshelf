157e18945c44028b30dd0f4edc2c01eff1c9872c
Sven <svenrisse0@gmail.com>
2024-04-15 19:39:00 +0200

    test(UserBook): add want to read test

--- internal/models/usersBooks_test.go
+++ internal/models/usersBooks_test.go
@@ -15,6 +15,7 @@ func TestUserBookModel_Insert(t *testing.T) {
 		Rating:     4.5,
 		ReviewBody: "Very good book!",
 		ReadAt:     time.Date(2024, 04, 12, 14, 30, 00, 0, time.UTC),
+		ReviewedAt: time.Date(2024, 04, 14, 18, 00, 00, 0, time.UTC),
 	}
 
 	tests := []struct {
@@ -23,6 +24,13 @@ func TestUserBookModel_Insert(t *testing.T) {
 		wantErr  string
 	}{
 		{name: "valid", userBook: validUserBook, wantErr: ""},
+		{name: "Want to read", userBook: UserBook{
+			BookID:     validUserBook.BookID,
+			UserID:     validUserBook.BookID,
+			Read:       false,
+			Rating:     0,
+			ReviewBody: "",
+		}, wantErr: ""},
 		{name: "Non-existent userID", userBook: UserBook{
 			BookID:     validUserBook.BookID,
 			UserID:     9,
