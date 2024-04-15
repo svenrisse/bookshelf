2ecae25660908b60e7a7755e2c564deafae88757
Sven <svenrisse0@gmail.com>
2024-04-15 16:18:11 +0200

    test(testdata): update setup and teardown

--- internal/models/testdata/setup.sql
+++ internal/models/testdata/setup.sql
@@ -24,4 +24,32 @@ CREATE TABLE IF NOT EXISTS users (
     provider text NOT NULL
 );
 
+CREATE TABLE IF NOT EXISTS reviews (
+   id bigserial PRIMARY KEY,
+   bookId bigserial,
+   userId bigserial,
+   rating FLOAT(2),
+   body text,
+   created_at timestamp(0) with time zone NOT NULL DEFAULT NOW(),
+   version integer NOT NULL DEFAULT 1,
+
+   FOREIGN KEY (bookId) REFERENCES books(id),
+   FOREIGN KEY (userId) REFERENCES users(id)
+ );
+
+CREATE TABLE IF NOT EXISTS usersBooksRelation (
+   bookId bigserial,
+   usersId bigserial, 
+   reviewId bigserial,
+   read BOOLEAN NOT NULL,
+   added_at timestamp(0) with time zone NOT NULL DEFAULT NOW(),
+   date_read timestamp(0) with time zone,
+
+   FOREIGN KEY (bookId) REFERENCES books(id),
+   FOREIGN KEY (usersId) REFERENCES users(id),
+   FOREIGN KEY (reviewId) REFERENCES reviews(id),
+   UNIQUE (bookId, usersId)
+ );
+
 INSERT INTO users (id, name, avatar, provider) VALUES (1, 'Alice Jones', 'avat', 'discord');
+INSERT INTO books (id, title, author, year, pages, genres) VALUES (1, 'The Hobbit', 'JRR Tolkien', 1890, 320, ARRAY ['Fantasy', 'Childrens Literature']);
--- internal/models/testdata/teardown.sql
+++ internal/models/testdata/teardown.sql
@@ -1,2 +1,4 @@
+DROP TABLE usersBooksRelation;
+DROP TABLE reviews;
 DROP TABLE users;
 DROP TABLE books;
