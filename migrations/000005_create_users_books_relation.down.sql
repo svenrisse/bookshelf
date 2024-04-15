4b56a3cda09c69c73a0dfcf05cca2116ac775ce1
Sven <svenrisse0@gmail.com>
2024-04-15 15:36:52 +0200

    migrations(5): add reviews

--- migrations/000005_create_users_books_relation.down.sql
+++ migrations/000005_create_users_books_relation.down.sql
@@ -1,1 +1,2 @@
 DROP TABLE IF EXISTS usersBooksRelation;
+DROP TABLE IF EXISTS reviews;
--- migrations/000005_create_users_books_relation.up.sql
+++ migrations/000005_create_users_books_relation.up.sql
@@ -1,11 +1,25 @@
+CREATE TABLE IF NOT EXISTS reviews (
+  id bigserial PRIMARY KEY,
+  bookId bigserial,
+  userId bigserial,
+  rating FLOAT(2),
+  body text,
+  created_at timestamp(0) with time zone NOT NULL DEFAULT NOW(),
+
+  FOREIGN KEY (bookId) REFERENCES books(id),
+  FOREIGN KEY (userId) REFERENCES users(id)
+);
+
 CREATE TABLE IF NOT EXISTS usersBooksRelation (
   bookId bigserial,
   usersId bigserial, 
+  reviewId bigserial,
   read BOOLEAN NOT NULL,
   added_at timestamp(0) with time zone NOT NULL DEFAULT NOW(),
   date_read timestamp(0) with time zone,
 
   FOREIGN KEY (bookId) REFERENCES books(id),
   FOREIGN KEY (usersId) REFERENCES users(id),
+  FOREIGN KEY (reviewId) REFERENCES reviews(id),
   UNIQUE (bookId, usersId)
 );
